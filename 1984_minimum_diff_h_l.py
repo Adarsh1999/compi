615a900c3fafeca9e4e953b2d486fc6aeda8cfca
Adarsh Gupta <adarshguptamaurya@gmail.com>
2024-03-13 18:25:22 +0000

    code

--- 1984_minimum_diff_h_l.py
+++ 1984_minimum_diff_h_l.py
@@ -1,7 +1,7 @@
 nums = [9,4,1,7]
 k = 2
 # Output: 2
-
+nums=sorted(nums)
 if k==1:
   print(0)
 l= 0
@@ -9,4 +9,7 @@ r=len(nums)-1
 
 diff=1000000
 while l<=r:
+  diff = min(diff,nums[r]-nums[l])
+  if diff
+
   
